420cdd9fee06c7f8db1069878b6371b446659210
Vikri Putra Permana <Vppermana808@gmail.com>
2023-10-26 14:44:40 +0700

    praktikum4

---
+++ app/Http/Controllers/AnimalController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AnimalController extends Controller
+{
+    public $animals = [
+        ["name" => "panda"],
+        ["name" => "nyamuk"],
+        ["name" => "ayam"]
+    ];
+    public function index()
+    {
+        foreach ($this->animals as $animal) {
+            echo "Nama Hewan : $animal[name] <br>";
+        }
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        rray_push($this->animals, $request);
+        $this->index();
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $this->animals[$id] = $request;
+        $this->index();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        # gunakan method unset atau array_splice untuk menghapus data array
+        unset($this->animals[$id]);
+        $this->index();
+    }
+}
