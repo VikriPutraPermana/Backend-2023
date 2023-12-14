7542421a7aa28b14d2f1df0fbc5ad1429b2c24e0
Vikri Putra Permana <Vppermana808@gmail.com>
2023-12-14 15:14:51 +0700

    praktikum10

---
+++ synchronous.js
@@ -0,0 +1,9 @@
+console.log("Vikri Membuka Bowser Google Chrome.");
+
+console.log(`
+Vikri download file selama 1 jam.
+  Downloading 1 Hour ...
+  Download Complete.
+`);
+
+console.log("Vikri membuka Youtube.");
